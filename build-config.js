46884a373fe76d5622cacfe554fd2afec779e278
Vivian Spencer <vivian@byinspiration.co.uk>
2015-07-16 16:33:28 +0100

    Updated gitignore to exclude jetbrains project folder and npm modules Installed gulp, gulp-htmlhint, gulp-inline-css, browser-sync, gulp-special-html, gulp-litmus, gulp-sass Created a ruleset for htmlhint Added litmus settings to a build config file

---
+++ build-config.js
@@ -0,0 +1,14 @@
+var config = {
+    litmus : {
+        username: 'litmus_username',
+        password: 'litmus_password',
+        url: 'https://yourcompany.litmus.com',
+        applications: [
+            'applemail6',
+            'gmailnew',
+            'ffgmailnew',
+            'chromegmailnew',
+            'iphone4s'
+        ] // Full list at https://yourcompany.litmus.com/emails/clients.xml
+    }
+}
