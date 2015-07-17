c9851d6dbe45bfd2d2215484e54960c025a9d436
Vivian Spencer <vivian@byinspiration.co.uk>
2015-07-17 16:30:51 +0100

    Renamed the build config file Updated Litmus email clients Setup the the build config file as a module

---
+++ build.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * This file/module contains all configuration for the build process.
+ */
+module.exports = {
+    buildDir: './build',
+    prodDir: './dist',
+
+    litmus: {
+        username: 'litmus_username',
+        password: 'litmus_password',
+        url: 'https://yourcompany.litmus.com',
+        applications: ['android4', 'aolonline', 'androidgmailapp', 'aolonline', 'ffaolonline',
+            'chromeaolonline', 'appmail6', 'iphone6', 'ipadmini', 'ipad', 'chromegmailnew',
+            'iphone6plus', 'notes85', 'ol2002', 'ol2003', 'ol2007', 'ol2010', 'ol2011',
+            'ol2013', 'outlookcom', 'chromeoutlookcom', 'chromeyahoo', 'windowsphone8'] // Full list at https://yourcompany.litmus.com/emails/clients.xml
+    }
+};
